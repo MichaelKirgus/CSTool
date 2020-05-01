1473cefbdc94316af364527ccf835ac25952b6ea
MichaelKirgus <git.michael@kirgus.net>
2020-05-01 20:05:36 +0200

    Add standalone builder library to spawn standalone application from main application

---
+++ CSToolPluginStandaloneHelper/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Allgemeine Informationen über eine Assembly werden über die folgenden
+' Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+' die einer Assembly zugeordnet sind.
+
+' Werte der Assemblyattribute überprüfen
+
+<Assembly: AssemblyTitle("CSToolPluginStandaloneHelper")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("CSToolPluginStandaloneHelper")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Die folgende GUID wird für die typelib-ID verwendet, wenn dieses Projekt für COM verfügbar gemacht wird.
+<Assembly: Guid("98ce639a-8cda-4bd7-9dd5-52814fd8b708")>
+
+' Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+'
+'      Hauptversion
+'      Nebenversion
+'      Buildnummer
+'      Revision
+'
+' Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+' indem Sie "*" wie unten gezeigt eingeben:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ CSToolPluginStandaloneHelper/StandaloneInstanceBuilder.vb
@@ -0,0 +1,41 @@
+﻿Public Class StandaloneInstanceBuilder
+    Public Function SpawnStandaloneInstance(ByVal PluginFilePath As String, ByVal PluginSettingFile As String, ByVal UserProfilePath As String, Optional ByVal EnvironmentPluginFolder As String = "", Optional ByVal CredentialPluginFolder As String = "", Optional IsNonPersistent As Boolean = False, Optional ByVal UserSetting As String = "Default", Optional ByVal InstanceTag As String = "", Optional InitialHostname As String = "", Optional ByVal WindowState As ProcessWindowStyle = ProcessWindowStyle.Normal) As Boolean
+        Try
+            Dim newhost As New Process
+            newhost.StartInfo.FileName = "CSToolPluginHost.exe"
+
+            Dim args As String = ""
+            If Not EnvironmentPluginFolder = "" Then
+                args += "/environmentplugindir " & My.Resources.pathsep & EnvironmentPluginFolder & My.Resources.pathsep & " "
+            End If
+            If Not CredentialPluginFolder = "" Then
+                args += "/credentialplugindir " & My.Resources.pathsep & CredentialPluginFolder & My.Resources.pathsep & " "
+            End If
+            args += "/plugin " & My.Resources.pathsep & PluginFilePath & My.Resources.pathsep & " "
+            args += "/pluginsettings " & My.Resources.pathsep & PluginSettingFile & My.Resources.pathsep & " "
+            args += "/userprofilepath " & My.Resources.pathsep & UserProfilePath & My.Resources.pathsep & " "
+            If IsNonPersistent Then
+                args += "/nonpersistent "
+            End If
+            If Not InstanceTag = "" Then
+                args += "/instancetag " & My.Resources.pathsep & InstanceTag & My.Resources.pathsep & " "
+            End If
+            If Not InitialHostname = "" Then
+                args += "/hostname " & My.Resources.pathsep & InitialHostname & My.Resources.pathsep & " "
+            End If
+
+            If args.EndsWith(" ") Then
+                args = args.Substring(0, args.Length - 1)
+            End If
+
+            newhost.StartInfo.WindowStyle = WindowState
+            If newhost.Start() Then
+                Return True
+            Else
+                Return False
+            End If
+        Catch ex As Exception
+            Return False
+        End Try
+    End Function
+End Class
