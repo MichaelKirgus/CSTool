3da67fc0f82eaa751bd2c26e2b2d0e5006c79486
MichaelKirgus <git.michael@kirgus.net>
2020-05-01 14:31:35 +0200

    Prepare CSTool host application

--- CSToolPluginHost/Form1.Designer.vb
+++ CSToolPluginHost/Form1.Designer.vb
@@ -1,4 +1,6 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿Imports WeifenLuo.WinFormsUI.Docking
+
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
@@ -22,14 +24,18 @@ Partial Class Form1
     'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
         Me.ToolStripLabel1 = New System.Windows.Forms.ToolStripLabel()
-        Me.ToolStripButton4 = New System.Windows.Forms.ToolStripButton()
-        Me.ToolStripButton1 = New System.Windows.Forms.ToolStripButton()
         Me.HostnameOrIPCtl = New System.Windows.Forms.ToolStripTextBox()
+        Me.ToolStripButton1 = New System.Windows.Forms.ToolStripButton()
         Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ToolStripButton4 = New System.Windows.Forms.ToolStripButton()
+        Me.CSDockPanelHosting = New WeifenLuo.WinFormsUI.Docking.DockPanel()
+        Me.VisualStudioToolStripExtender1 = New WeifenLuo.WinFormsUI.Docking.VisualStudioToolStripExtender(Me.components)
         Me.ToolStrip1.SuspendLayout()
+        CType(Me.CSDockPanelHosting, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'ToolStrip1
@@ -51,15 +57,14 @@ Partial Class Form1
         Me.ToolStripLabel1.Size = New System.Drawing.Size(85, 26)
         Me.ToolStripLabel1.Text = "Hostname/IP:"
         '
-        'ToolStripButton4
+        'HostnameOrIPCtl
         '
-        Me.ToolStripButton4.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
-        Me.ToolStripButton4.Image = Global.CSToolPluginHost.My.Resources.Resources.icon_refresh_22x22
-        Me.ToolStripButton4.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None
-        Me.ToolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta
-        Me.ToolStripButton4.Name = "ToolStripButton4"
-        Me.ToolStripButton4.Size = New System.Drawing.Size(26, 26)
-        Me.ToolStripButton4.Text = "Refresh"
+        Me.HostnameOrIPCtl.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.HostnameOrIPCtl.Name = "HostnameOrIPCtl"
+        Me.HostnameOrIPCtl.Size = New System.Drawing.Size(159, 29)
+        Me.HostnameOrIPCtl.ToolTipText = "Please enter hostname or ip-address." & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Shortcuts:" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "ENTF: Clear hostname/ip" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "F5: " &
+    "Refresh GUI" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "F9: Open new instance with entered hostname/ip" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "F10: Open new windo" &
+    "w with entered hostname/ip"
         '
         'ToolStripButton1
         '
@@ -71,31 +76,49 @@ Partial Class Form1
         Me.ToolStripButton1.Size = New System.Drawing.Size(26, 26)
         Me.ToolStripButton1.Text = "Get information"
         '
-        'HostnameOrIPCtl
-        '
-        Me.HostnameOrIPCtl.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.HostnameOrIPCtl.Name = "HostnameOrIPCtl"
-        Me.HostnameOrIPCtl.Size = New System.Drawing.Size(159, 29)
-        Me.HostnameOrIPCtl.ToolTipText = "Please enter hostname or ip-address." & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Shortcuts:" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "ENTF: Clear hostname/ip" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "F5: " &
-    "Refresh GUI" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "F9: Open new instance with entered hostname/ip" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "F10: Open new windo" &
-    "w with entered hostname/ip"
-        '
         'ToolStripSeparator1
         '
         Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
         Me.ToolStripSeparator1.Size = New System.Drawing.Size(6, 29)
+        '
+        'ToolStripButton4
+        '
+        Me.ToolStripButton4.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.ToolStripButton4.Image = Global.CSToolPluginHost.My.Resources.Resources.icon_refresh_22x22
+        Me.ToolStripButton4.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None
+        Me.ToolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton4.Name = "ToolStripButton4"
+        Me.ToolStripButton4.Size = New System.Drawing.Size(26, 26)
+        Me.ToolStripButton4.Text = "Refresh"
+        '
+        'CSDockPanelHosting
+        '
+        Me.CSDockPanelHosting.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.CSDockPanelHosting.DockBackColor = System.Drawing.Color.FromArgb(CType(CType(45, Byte), Integer), CType(CType(45, Byte), Integer), CType(CType(48, Byte), Integer))
+        Me.CSDockPanelHosting.Location = New System.Drawing.Point(0, 29)
+        Me.CSDockPanelHosting.Name = "CSDockPanelHosting"
+        Me.CSDockPanelHosting.Size = New System.Drawing.Size(933, 545)
+        Me.CSDockPanelHosting.TabIndex = 2
+        Me.CSDockPanelHosting.Theme = Nothing
+        '
+        'VisualStudioToolStripExtender1
+        '
+        Me.VisualStudioToolStripExtender1.DefaultRenderer = Nothing
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(933, 574)
+        Me.Controls.Add(Me.CSDockPanelHosting)
         Me.Controls.Add(Me.ToolStrip1)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.IsMdiContainer = True
         Me.Name = "Form1"
+        Me.Text = "CSTool"
         Me.ToolStrip1.ResumeLayout(False)
         Me.ToolStrip1.PerformLayout()
+        CType(Me.CSDockPanelHosting, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -107,4 +130,6 @@ Partial Class Form1
     Friend WithEvents ToolStripButton1 As ToolStripButton
     Friend WithEvents ToolStripButton4 As ToolStripButton
     Friend WithEvents ToolStripSeparator1 As ToolStripSeparator
+    Friend WithEvents CSDockPanelHosting As DockPanel
+    Friend WithEvents VisualStudioToolStripExtender1 As VisualStudioToolStripExtender
 End Class
--- CSToolPluginHost/Form1.vb
+++ CSToolPluginHost/Form1.vb
@@ -1,4 +1,56 @@
-﻿Public Class Form1
+﻿Imports CSToolApplicationSettingsLib
+Imports CSToolApplicationSettingsManager
+Imports CSToolEnvironmentManager
+Imports CSToolLogLib
+Imports CSToolWindowManager
+
+Public Class Form1
+    Public LogManager As New LogLib
+    Public WindowManagerHandler As New WindowManager
+    Public ApplicationSettingManager As New ApplicationSettingsManager
+    Public ApplicationSettings As New ApplicationSettings
+    Public EnvironmentManager As New EnvironmentManager
+
+    Public PluginFilePath As String = ""
+    Public UserWorkspaceSetting As String = "Default"
+
+    Public Function ProccessCommandLineArgsAppBase(ByVal CmdArgs As String()) As Boolean
+        Try
+            Dim arglist As List(Of String)
+            arglist = CmdArgs.ToList
+
+            For ind = 0 To arglist.Count - 1
+                If arglist(ind).ToLower = "/logtofile" Then
+                    LogManager.LogSettings.LogToFile = True
+                    LogManager.LogSettings.LogToFilePath = (arglist(ind + 1))
+                    LogManager.ReInitLogSystem()
+                End If
+                If arglist(ind).ToLower = "/logeventlog" Then
+                    LogManager.LogSettings.LogToEventLog = True
+                    LogManager.ReInitLogSystem()
+                End If
+                If arglist(ind).ToLower = "/environmentplugindir" Then
+                    Dim EnvDirInfo As New IO.DirectoryInfo(arglist(ind + 1))
+                    WindowManagerHandler.EnvironmentPluginDir = EnvDirInfo.FullName
+                End If
+                If arglist(ind).ToLower = "/credentialplugindir" Then
+                    Dim CredDirInfo As New IO.DirectoryInfo(arglist(ind + 1))
+                    WindowManagerHandler.CredentialPluginDir = CredDirInfo.FullName
+                End If
+                If arglist(ind).ToLower = "/plugin" Then
+                    PluginFilePath = (arglist(ind + 1))
+                End If
+                If arglist(ind).ToLower = "/workspace" Then
+                    UserWorkspaceSetting = (arglist(ind + 1))
+                End If
+            Next
+
+            Return True
+        Catch ex As Exception
+            Return False
+        End Try
+    End Function
+
     Private Sub HostnameOrIPCtl_KeyDown(sender As Object, e As KeyEventArgs)
 
     End Sub
@@ -16,6 +68,15 @@
     End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LogManager.InitLogSystem()
+        WindowManagerHandler._LogManager = LogManager
+        ProccessCommandLineArgsAppBase(Environment.GetCommandLineArgs)
+        Dim EnvDirInfo As New IO.DirectoryInfo(ApplicationSettings.EnvironmentPluginDir)
+        Dim CredDirInfo As New IO.DirectoryInfo(ApplicationSettings.CredentialPluginDir)
+        WindowManagerHandler.EnvironmentPluginDir = EnvDirInfo.FullName
+        WindowManagerHandler.CredentialPluginDir = CredDirInfo.FullName
+        WindowManagerHandler.PluginManager.LoadPluginFile(PluginFilePath)
+        WindowManagerHandler._DockingContent = CSDockPanelHosting
 
     End Sub
 End Class
