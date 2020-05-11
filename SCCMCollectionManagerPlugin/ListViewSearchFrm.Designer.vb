cad50b1b0f6ab310f88e1d20fc76c64d67ae6e33
MichaelKirgus <git.michael@kirgus.net>
2020-05-11 22:51:57 +0200

    Improve collection search window

--- SCCMCollectionManagerPlugin/ListViewSearchFrm.Designer.vb
+++ SCCMCollectionManagerPlugin/ListViewSearchFrm.Designer.vb
@@ -30,7 +30,16 @@ Partial Class ListViewSearchFrm
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.ResultCountLbl = New System.Windows.Forms.Label()
         Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
         Me.SuspendLayout()
         '
         'GroupBox1
@@ -41,7 +50,7 @@ Partial Class ListViewSearchFrm
         Me.GroupBox1.Size = New System.Drawing.Size(360, 44)
         Me.GroupBox1.TabIndex = 0
         Me.GroupBox1.TabStop = False
-        Me.GroupBox1.Text = "Search"
+        Me.GroupBox1.Text = "Search text"
         '
         'TextBox1
         '
@@ -53,19 +62,107 @@ Partial Class ListViewSearchFrm
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(293, 54)
+        Me.Button1.Location = New System.Drawing.Point(293, 173)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 1
         Me.Button1.Text = "&Search"
         Me.Button1.UseVisualStyleBackColor = True
         '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.CheckBox2)
+        Me.GroupBox2.Controls.Add(Me.ComboBox1)
+        Me.GroupBox2.Controls.Add(Me.Label1)
+        Me.GroupBox2.Controls.Add(Me.RadioButton2)
+        Me.GroupBox2.Controls.Add(Me.RadioButton1)
+        Me.GroupBox2.Controls.Add(Me.CheckBox1)
+        Me.GroupBox2.Location = New System.Drawing.Point(8, 54)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(360, 113)
+        Me.GroupBox2.TabIndex = 2
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Settings"
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(6, 42)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(96, 17)
+        Me.CheckBox1.TabIndex = 0
+        Me.CheckBox1.Text = "Case-Sensitive"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Checked = True
+        Me.RadioButton1.Location = New System.Drawing.Point(6, 19)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(108, 17)
+        Me.RadioButton1.TabIndex = 1
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Item contains text"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(148, 19)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(84, 17)
+        Me.RadioButton2.TabIndex = 2
+        Me.RadioButton2.Text = "Exact match"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(3, 68)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(87, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Search in Group:"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"All groups"})
+        Me.ComboBox1.Location = New System.Drawing.Point(6, 84)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(348, 21)
+        Me.ComboBox1.TabIndex = 4
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Checked = True
+        Me.CheckBox2.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox2.Location = New System.Drawing.Point(148, 42)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(89, 17)
+        Me.CheckBox2.TabIndex = 5
+        Me.CheckBox2.Text = "Select results"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'ResultCountLbl
+        '
+        Me.ResultCountLbl.AutoSize = True
+        Me.ResultCountLbl.Location = New System.Drawing.Point(11, 178)
+        Me.ResultCountLbl.Name = "ResultCountLbl"
+        Me.ResultCountLbl.Size = New System.Drawing.Size(0, 13)
+        Me.ResultCountLbl.TabIndex = 3
+        '
         'ListViewSearchFrm
         '
         Me.AcceptButton = Me.Button1
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(376, 84)
+        Me.ClientSize = New System.Drawing.Size(376, 204)
+        Me.Controls.Add(Me.ResultCountLbl)
+        Me.Controls.Add(Me.GroupBox2)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.GroupBox1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
@@ -77,11 +174,22 @@ Partial Class ListViewSearchFrm
         Me.Text = "Search item in collection"
         Me.GroupBox1.ResumeLayout(False)
         Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents GroupBox1 As GroupBox
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents Button1 As Button
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents ResultCountLbl As Label
 End Class
--- SCCMCollectionManagerPlugin/ListViewSearchFrm.vb
+++ SCCMCollectionManagerPlugin/ListViewSearchFrm.vb
@@ -9,27 +9,154 @@ Public Class ListViewSearchFrm
 
     Public ListViewCtl As ListView
 
+    Public Sub LoadGroupItems()
+        Try
+            If Not ListViewCtl.Groups.Count = 0 Then
+                For index = 0 To ListViewCtl.Groups.Count - 1
+                    ComboBox1.Items.Add(ListViewCtl.Groups(index).Header)
+                Next
+            End If
+
+            ComboBox1.SelectedIndex = 0
+        Catch ex As Exception
+        End Try
+    End Sub
+
+
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Try
             Dim result As Boolean = False
             Dim FirstFoundItemIndex As Integer = 0
+            Dim resultcount As Integer = 0
 
-            For ind = 0 To ListViewCtl.Items.Count - 1
-                If ListViewCtl.Items(ind).Text.ToLower.Contains(TextBox1.Text) Then
-                    ListViewCtl.Items(ind).Selected = True
-                    If result = False Then
-                        FirstFoundItemIndex = ind
+            If ComboBox1.SelectedIndex = 0 Then
+                If RadioButton1.Checked Then
+                    If CheckBox1.Checked Then
+                        For ind = 0 To ListViewCtl.Items.Count - 1
+                            If ListViewCtl.Items(ind).Text.Contains(TextBox1.Text) Then
+                                If CheckBox2.Checked Then
+                                    ListViewCtl.Items(ind).Selected = True
+                                End If
+                                If result = False Then
+                                    FirstFoundItemIndex = ind
+                                End If
+                                result = True
+                                resultcount += 1
+                            End If
+                        Next
+                    Else
+                        For ind = 0 To ListViewCtl.Items.Count - 1
+                            If ListViewCtl.Items(ind).Text.ToLower.Contains(TextBox1.Text.ToLower) Then
+                                If CheckBox2.Checked Then
+                                    ListViewCtl.Items(ind).Selected = True
+                                End If
+                                If result = False Then
+                                    FirstFoundItemIndex = ind
+                                End If
+                                result = True
+                                resultcount += 1
+                            End If
+                        Next
                     End If
-                    result = True
+                Else
+                    If CheckBox1.Checked Then
+                        For ind = 0 To ListViewCtl.Items.Count - 1
+                            If ListViewCtl.Items(ind).Text = TextBox1.Text Then
+                                If CheckBox2.Checked Then
+                                    ListViewCtl.Items(ind).Selected = True
+                                End If
+                                If result = False Then
+                                    FirstFoundItemIndex = ind
+                                End If
+                                result = True
+                                resultcount += 1
+                            End If
+                        Next
+                    Else
+                        For ind = 0 To ListViewCtl.Items.Count - 1
+                            If ListViewCtl.Items(ind).Text.ToLower = TextBox1.Text.ToLower Then
+                                If CheckBox2.Checked Then
+                                    ListViewCtl.Items(ind).Selected = True
+                                End If
+                                If result = False Then
+                                    FirstFoundItemIndex = ind
+                                End If
+                                result = True
+                                resultcount += 1
+                            End If
+                        Next
+                    End If
                 End If
-            Next
+            Else
+                For ind = 0 To ListViewCtl.Items.Count - 1
+                    If ListViewCtl.Items(ind).Group.Header = ListViewCtl.Groups(ComboBox1.SelectedIndex - 1).Header Then
+                        If RadioButton1.Checked Then
+                            If CheckBox1.Checked Then
+                                If ListViewCtl.Items(ind).Text.Contains(TextBox1.Text) Then
+                                    If CheckBox2.Checked Then
+                                        ListViewCtl.Items(ind).Selected = True
+                                    End If
+                                    If result = False Then
+                                        FirstFoundItemIndex = ind
+                                    End If
+                                    result = True
+                                    resultcount += 1
+                                End If
+                            Else
+                                If ListViewCtl.Items(ind).Text.ToLower.Contains(TextBox1.Text.ToLower) Then
+                                    If CheckBox2.Checked Then
+                                        ListViewCtl.Items(ind).Selected = True
+                                    End If
+                                    If result = False Then
+                                        FirstFoundItemIndex = ind
+                                    End If
+                                    result = True
+                                    resultcount += 1
+                                End If
+                            End If
+                        Else
+                            If CheckBox1.Checked Then
+                                If ListViewCtl.Items(ind).Text = TextBox1.Text Then
+                                    If CheckBox2.Checked Then
+                                        ListViewCtl.Items(ind).Selected = True
+                                    End If
+                                    If result = False Then
+                                        FirstFoundItemIndex = ind
+                                    End If
+                                    result = True
+                                    resultcount += 1
+                                End If
+                            Else
+                                If ListViewCtl.Items(ind).Text.ToLower = TextBox1.Text.ToLower Then
+                                    If CheckBox2.Checked Then
+                                        ListViewCtl.Items(ind).Selected = True
+                                    End If
+                                    If result = False Then
+                                        FirstFoundItemIndex = ind
+                                    End If
+                                    result = True
+                                    resultcount += 1
+                                End If
+                            End If
+                        End If
+
+                    End If
+                Next
+            End If
 
             If result = False Then
-                MsgBox("Keine Ergebnisse gefunden!", MsgBoxStyle.Information)
+                ResultCountLbl.Text = "No results found."
+                MsgBox("No results found!", MsgBoxStyle.Information)
             Else
+                ResultCountLbl.Text = resultcount & " result(s) found."
                 ListViewCtl.EnsureVisible(FirstFoundItemIndex)
             End If
         Catch ex As Exception
+            MsgBox("Error searching.", MsgBoxStyle.Critical)
         End Try
+    End Sub
+
+    Private Sub ListViewSearchFrm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LoadGroupItems()
     End Sub
 End Class
