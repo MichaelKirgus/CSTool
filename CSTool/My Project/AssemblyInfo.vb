115299acc01f9c3684e46b11cd27640657de1ca4
MichaelKirgus <github.michael@kirgus.net>
2020-08-22 19:24:59 +0200

    Update assembly version

--- CSTool/My Project/AssemblyInfo.vb
+++ CSTool/My Project/AssemblyInfo.vb
@@ -36,5 +36,5 @@ Imports System.Runtime.InteropServices
 ' indem Sie "*" wie unten gezeigt eingeben:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("2.0.0.14")>
-<Assembly: AssemblyFileVersion("2.0.0.14")>
+<Assembly: AssemblyVersion("2.0.0.15")>
+<Assembly: AssemblyFileVersion("2.0.0.15")>
