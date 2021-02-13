268b196500499181088d517f6903a3a50eb3620b
Michael Kirgus <github.michael@kirgus.net>
2021-02-13 23:24:08 +0100

    Update license header

--- CSToolPluginStandaloneHelper/StandaloneInstanceBuilder.vb
+++ CSToolPluginStandaloneHelper/StandaloneInstanceBuilder.vb
@@ -1,4 +1,9 @@
-﻿Public Class StandaloneInstanceBuilder
+﻿'Copyright (C) 2019-2021 Michael Kirgus
+'This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+'This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+'You should have received a copy of the GNU General Public License along with this program; if not, see <https://www.gnu.org/licenses>.
+'Additional copyright notices in project base directory or main executable directory.
+Public Class StandaloneInstanceBuilder
     Public Function SpawnStandaloneInstance(ByVal PluginFilePath As String, ByVal PluginSettingFile As String, ByVal UserProfilePath As String, Optional ByVal EnvironmentPluginFolder As String = "", Optional ByVal CredentialPluginFolder As String = "", Optional IsNonPersistent As Boolean = False, Optional ByVal UserSetting As String = "Default", Optional ByVal InstanceTag As String = "", Optional InitialHostname As String = "", Optional ByVal WindowState As ProcessWindowStyle = ProcessWindowStyle.Normal) As Boolean
         Try
             Dim newhost As New Process
